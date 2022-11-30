8930cdc5111da7cd9f5eec110d7e2c77999b9eff
TheoDosSantos <theo@callbruno.com>
2022-11-30 10:50:38 +0100

    Update - Staring page - update style

--- src/View/Components/input/input.js
+++ src/View/Components/input/input.js
@@ -20,27 +20,28 @@ function Input() {
         ****************************************************************/
   return (
     <form
-      className="bg-gray-900 flex w-48"
+      className="bg-gray-900 w-full p-6 rounded-lg border border-gray-700"
       onSubmit={() => {
         navigate(`/summonerInfo/${summonerNameInput}`);
       }}
       method="GET"
     >
+      <div class="text-gray-200 text-md pb-2">Pseudo</div>
       <input
         id="inputhome"
-        className="w-6/12 h-10 rounded-tl-[15px] rounded-tr-[15px] m-2 p-2"
+        className="rounded-md h-10 bg-gray-800 px-4 w-full border border-gray-700 mb-6"
         type="text"
         name="summonerName"
         placeholder="Entrez un nom de joueur"
         value={summonerNameInput}
         onChange={(e) => handleChange(e)}
       />
-      <button
-        type="submit"
-        className="w-6/12 bg-amber-300 rounded-br-[15px] rounded-bl-[15px] m-2 p-2"
-      >
-        Recherche
-      </button>
+      <div className="container-btn flex justify-end">
+        <button type="submit" className="flex gap-2 items-center text-gray-200 bg-blue-600 px-4 py-2 rounded-md">
+          See the matches
+          <img class="w-[12px] rotate-180" src="/icon.svg" alt="" />
+        </button>
+      </div>
     </form>
   );
 }
--- src/View/Pages/Home/Home.js
+++ src/View/Pages/Home/Home.js
@@ -7,8 +7,33 @@ function Home() {
                                 RENDER
         ****************************************************************/
   return (
-    <div className="bgfullscreen flex flex-col justify-center items-center h-screen">
-      <Input />
+    <div className="container-landing bg-gray-800">
+      <div className="flex flex-col py-28 items-center">
+        <h1 className="text-4xl font-bold text-gray-200 mb-10">Projet LOL</h1>
+        <div className="flex flex-col w-full justify-center items-center mb-16">
+          <Input />
+        </div>
+        <div className="container-tutorial w-full">
+          <h2 className="text-2xl font-bold text-gray-200 mb-6">How does it work?</h2>
+          <div className="container-cards flex justify-between">
+            <div className="flex flex-col items-center w-64 bg-gray-900 p-6 rounded-lg">
+              <div className="w-12 h-12 bg-blue-600 rounded-full flex justify-center text-center items-center text-gray-200 mb-6">1</div>
+              <p className="text-gray-200 text-lg mb-6">Enter a pseudo</p>
+              <div><img src="/tuto-1.svg" alt="" /></div>
+            </div>
+            <div className="flex flex-col items-center w-64 bg-gray-900 p-6 rounded-lg">
+              <div className="w-12 h-12 bg-blue-600 rounded-full flex justify-center text-center items-center text-gray-200 mb-6">2</div>
+              <p className="text-gray-200 text-lg mb-6">Consult the matchs</p>
+              <div><img src="/tuto-2.svg" alt="" /></div>
+            </div>
+            <div className="flex flex-col items-center w-64 bg-gray-900 p-6 rounded-lg">
+              <div className="w-12 h-12 bg-blue-600 rounded-full flex justify-center text-center items-center text-gray-200 mb-6">3</div>
+              <p className="text-gray-200 text-lg mb-6 text-center">Access to the match’ heat map!</p>
+              <div><img src="/tuto-3.svg" alt="" /></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
