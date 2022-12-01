deb531313c4eb91e407e3472615b3ede47446099
doriandel <doriandelorme@outlook.fr>
2022-12-01 11:59:08 +0100

    updated the style of teams blocks

--- src/View/Components/team/team.js
+++ src/View/Components/team/team.js
@@ -2,68 +2,68 @@ import React from "react";
 import "./team.css";
 
 function team(props) {
-    const { team1, team2 } = props;
+  const { team1, team2 } = props;
 
-    return (
-        <div className="flex flex-row w-full">
-            <div className="flex flex-col w-full h-[200px] px-2 border-2 border-amber-400 m-1 bg-red-800">
-                <h2 className="text-center font-bold text-2xl text-amber-400">Team 1 :</h2>
-                <ol className="flex-col">
-                {team1 !== null ? (
-                        team1.map((participant) => {
-                            return (
-                                <li
-                                    className="flex space-x-2 items-center justify-start"
-                                    key={participant[2]}
-                                >
-                                    <div className="flex-shrink-0 ml-2">
-                                        <img
-                                            className="h-6 w-6 rounded-md"
-                                            src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/${participant[1]}.png`}
-                                            alt="img champion"
-                                        />
-                                    </div>
-                                    <p className="text-xl text-gray-50">
-                                        {participant[0]}
-                                    </p>
-                                </li>
-                            );
-                        })
-                    ) : (
-                        <p>Chargement ...</p>
-                    )}
-                </ol>
-            </div>
-            <div className="flex flex-col w-full h-[200px] border-2 border-amber-400 m-1 bg-blue-800">
-                <h2 className="text-center font-bold text-2xl text-amber-400">Team 2 :</h2>
-                <ol className="flex-col">
-                    {team2 !== null ? (
-                        team2.map((participant) => {
-                            return (
-                                <li
-                                    className="flex space-x-2 items-center justify-start"
-                                    key={participant[2]}
-                                >
-                                    <div className="flex-shrink-0 ml-2">
-                                        <img
-                                            className="h-6 w-6 rounded-md"
-                                            src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/${participant[1]}.png`}
-                                            alt="img champion"
-                                        />
-                                    </div>
-                                    <p className="text-xl text-gray-50">
-                                        {participant[0]}
-                                    </p>
-                                </li>
-                            );
-                        })
-                    ) : (
-                        <p>Chargement ...</p>
-                    )}
-                </ol>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-row w-full">
+      <div className="flex flex-col w-full px-2 gap-4">
+        <h2 className="font-bold text-2xl text-blue-500">Team Blue</h2>
+        <ol className="flex-col p-4 bg-gray-800 rounded-xl space-y-2">
+          {team1 !== null ? (
+            team1.map((participant) => {
+              return (
+                <li
+                  className="flex space-x-2 items-center justify-start"
+                  key={participant[2]}
+                >
+                  <div className="flex-shrink-0 ml-2">
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/${participant[1]}.png`}
+                      alt="img champion"
+                    />
+                  </div>
+                  <p className="text-xl text-blue-500 font-medium">
+                    {participant[0]}
+                  </p>
+                </li>
+              );
+            })
+          ) : (
+            <p>Chargement ...</p>
+          )}
+        </ol>
+      </div>
+      <div className="flex flex-col w-full px-2 gap-4">
+        <h2 className="font-bold text-2xl text-red-500">Team Red</h2>
+        <ol className="flex-col p-4 bg-gray-800 rounded-xl space-y-2">
+          {team2 !== null ? (
+            team2.map((participant) => {
+              return (
+                <li
+                  className="flex space-x-2 items-center justify-start"
+                  key={participant[2]}
+                >
+                  <div className="flex-shrink-0 ml-2">
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/${participant[1]}.png`}
+                      alt="img champion"
+                    />
+                  </div>
+                  <p className="text-xl text-red-500 font-medium">
+                    {participant[0]}
+                  </p>
+                </li>
+              );
+            })
+          ) : (
+            <p>Chargement ...</p>
+          )}
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default team;
